68859c92ab0800cf3e37bec8d4b16c285ae87260
philiplinden <lindenphilipj@gmail.com>
2024-02-16 22:49:50 -0500

    Update quartz.layout.ts

--- .site-config/quartz.layout.ts
+++ .site-config/quartz.layout.ts
@@ -7,7 +7,7 @@ export const sharedPageComponents: SharedLayout = {
     header: [],
     footer: Component.Footer({
         links: {
-            GitHub: "https://github.com/philiplinden/symbaroum"
+            GitHub: "https://github.com/philiplinden/symbaroum-vault"
         },
     }),
 }
